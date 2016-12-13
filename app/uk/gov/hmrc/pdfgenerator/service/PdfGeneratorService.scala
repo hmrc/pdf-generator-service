0497a6983d84c0d3bdb4e648165f25d6e60ceee9
William Hayward <william.hayward@digital.hmrc.gov.uk>
2016-12-13 11:34:16 +0000

    added .ps contents

--- app/uk/gov/hmrc/pdfgenerator/service/PdfGeneratorService.scala
+++ app/uk/gov/hmrc/pdfgenerator/service/PdfGeneratorService.scala
@@ -77,7 +77,9 @@ trait PdfGeneratorService {
       val command: String = "gs -dPDFA=1 -dPDFACompatibilityPolicy=1  -dNOOUTERSAVE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -o " + outputFileName + " " + pDFAdef_path + " " + inputFileName
       Logger.info("GS command is " + command)
       val pb = Process(command)
-      val exitCode = pb.!
+      val exitCode = pb.!!
+      
+      Logger.info(exitCode)
 
       new File(outputFileName)
     }
