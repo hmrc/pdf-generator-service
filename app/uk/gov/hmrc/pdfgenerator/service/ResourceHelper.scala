dbbc3fabec1657402dc180f00c5c23cc9a53644a
PeteA <pricedtree@gmail.com>
2016-12-14 13:36:48 +0000

    IMplemented a Resource helper

---
+++ app/uk/gov/hmrc/pdfgenerator/service/ResourceHelper.scala
@@ -0,0 +1,22 @@
+package uk.gov.hmrc.pdfgenerator.service
+
+import java.io.{BufferedInputStream, BufferedOutputStream, FileOutputStream}
+
+/**
+  * Created by peter on 14/12/2016.
+  */
+class ResourceHelper {
+
+  def reader(filename: String): Array[Byte]  = {
+    val bis = new BufferedInputStream(getClass.getResourceAsStream(filename))
+    try Stream.continually(bis.read).takeWhile(-1 !=).map(_.toByte).toArray
+    finally bis.close()
+  }
+
+  def writer(filename: String, byteArray: Array[Byte]) = {
+    val bos = new BufferedOutputStream(new FileOutputStream(filename))
+    Stream.continually(bos.write(byteArray))
+    bos.close()
+  }
+
+}
