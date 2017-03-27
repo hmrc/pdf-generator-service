f073ae78e38e2872a47ed0fd7d44a394e8e2aa32
Pete Anning <peter.anning@gmail.com>
2017-03-27 13:57:42 +0100

    Added tests for setup files

--- app/uk/gov/hmrc/pdfgenerator/service/ResourceHelper.scala
+++ app/uk/gov/hmrc/pdfgenerator/service/ResourceHelper.scala
@@ -5,6 +5,7 @@ import java.io._
 import play.api.Logger
 
 import scala.io.Source
+import scala.util.{Failure, Success, Try}
 
 object ResourceHelper {
   def apply: ResourceHelper = new ResourceHelper()
@@ -12,47 +13,62 @@ object ResourceHelper {
 
 class ResourceHelper {
 
-  def reader(filename: String): Array[Byte]  = {
-    val bis = new BufferedInputStream(getClass.getResourceAsStream(filename))
-    try Stream.continually(bis.read).takeWhile(-1 !=).map(_.toByte).toArray
-    finally bis.close()
-  }
+  val failureMessage = "Generation of pdfs will most likely fail!"
 
-  def writer(filename: String, byteArray: Array[Byte]) = {
-    val bos = new BufferedOutputStream(new FileOutputStream(filename))
-    Stream.continually(bos.write(byteArray))
-    bos.close()
-  }
-
-
-  def setUpPsDefFile(pdDefFileBare: String,
-                     pdDefFileFullPath: String, baserDir: String,
-                     colorProfileBare: String,
-                     colorProfileFullPath: String) = {
+  def setupExecutableSupportFiles(psDefFileBare: String,
+                                  psDefFileFullPath: String, baserDir: String,
+                                  colorProfileBare: String,
+                                  colorProfileFullPath: String): Unit = {
 
     def replace(line: String): String = line.replace("$COLOUR_PROFILE$", colorProfileFullPath)
 
-      Logger.debug(s"Filtering pdf ${baserDir}conf/${pdDefFileBare}")
-      val source = Source fromFile baserDir + "conf/" + pdDefFileBare
+      Logger.debug(s"Filtering pdf ${baserDir}conf/${psDefFileBare}")
+      val source = Source fromFile baserDir + "conf/" + psDefFileBare
       val lines = source.getLines
       val result = lines.map(line => replace(line))
 
-      val file = new File(pdDefFileFullPath)
+      val file = new File(psDefFileFullPath)
       val bw = new BufferedWriter(new FileWriter(file))
 
       bw.write(result.mkString("\n"))
+
+      if(!file.exists) {
+        Logger.error(s"${psDefFileFullPath} does not exist! ${failureMessage}")
+      }
+
       bw.close()
       source.close()
 
-      setUpConfigFile(colorProfileBare, colorProfileFullPath)
+      copyFileAsBytes(colorProfileBare, colorProfileFullPath)
 
   }
 
-  private def setUpConfigFile(sourceFile: String, destinationFile: String): Unit = {
+  private def copyFileAsBytes(sourceFile: String, destinationFile: String): Unit = {
     if (!new File(destinationFile).exists) {
       Logger.debug(s"Byte Copying ${sourceFile} to ${destinationFile}")
       val bytes = reader("/" + sourceFile)
       writer(destinationFile, bytes)
+    }
+  }
+
+  private def reader(filename: String): Try[Array[Byte]]  = {
+    val bis = new BufferedInputStream(getClass.getResourceAsStream(filename))
+    val triedByteArray =  Try (Stream.continually(bis.read).takeWhile(-1 !=).map(_.toByte).toArray)
+    bis.close()
+    Logger.info(s"reading bytes for ${filename} : successful = ${triedByteArray.isSuccess}")
+    triedByteArray
+  }
+
+  private def writer(filename: String, byteArray: Try[Array[Byte]]) = {
+    val bos = new BufferedOutputStream(new FileOutputStream(filename))
+    val tried = byteArray.map( byteArray => Stream.continually(bos.write(byteArray)))
+    bos.close()
+
+    tried match  {
+      case Success(t) => Logger.info(s"Successfully wrote ${filename}")
+      case Failure(e) => {
+        Logger.error(s"Failed to write bytes for ${filename} error was ${e.getMessage}, ")
+      }
     }
   }
 
