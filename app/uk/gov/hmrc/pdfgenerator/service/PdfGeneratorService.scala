de78b4db54c525282a1dd43a4d386d3c0658d06a
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2019-02-20 22:37:18 +0000

    DDCNOS-906

--- app/uk/gov/hmrc/pdfgenerator/service/PdfGeneratorService.scala
+++ app/uk/gov/hmrc/pdfgenerator/service/PdfGeneratorService.scala
@@ -63,7 +63,7 @@ class PdfGeneratorService @Inject()(configuration: Configuration, resourceHelper
   }
 
   val GS_ALIAS: String = {
-    default(configuration, "gsAlias", "./"+getEnvironmentPath("gs-920-linux_x86_64"))
+    default(configuration, "gsAlias", getBaseDir+getEnvironmentPath("gs-920-linux_x86_64"))
   }
   val BASE_DIR: String = default(configuration, "baseDir", getBaseDir)
   val CONF_DIR: String = default(configuration, "confDir", getBaseDir)
@@ -155,7 +155,7 @@ class PdfGeneratorService @Inject()(configuration: Configuration, resourceHelper
 
   private def convertToPdfA(inputFileName: String, outputFileName: String): Try[File] = {
 
-    val command: String = GS_ALIAS + " -dPDFA=1 -dPDFACompatibilityPolicy=1  -dNOOUTERSAVE -sProcessColorModel=DeviceRGB " +
+    val command: String = GS_ALIAS.replaceAll(" ", "\\ ") + " -dPDFA=1 -dPDFACompatibilityPolicy=1  -dNOOUTERSAVE -sProcessColorModel=DeviceRGB " +
       "-sDEVICE=pdfwrite -o '" + outputFileName + "' " + PS_DEF_FILE_FULL_PATH + "  '" + inputFileName + "'"
 
     Logger.debug(s"Running: ${command}")
