7965a2515d7b54c0ea86f56f35866fdd48b629b2
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2019-03-11 15:19:39 +0000

    OSXFIX | all os will need a local version to run

--- app/uk/gov/hmrc/pdfgenerator/service/PdfGeneratorService.scala
+++ app/uk/gov/hmrc/pdfgenerator/service/PdfGeneratorService.scala
@@ -35,7 +35,7 @@ class PdfGeneratorService @Inject()(configuration: Configuration, resourceHelper
       val value = configuration.getString(CONFIG_KEY + key).getOrElse(productionDefault)
       value match {
         case EMPTY_INDICATOR => productionDefault
-        case _: String => if (new File(value).exists()) value else productionDefault
+        case _: String => value
       }
     } match {
       case Success(value) => value
