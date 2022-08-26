0d096a5551c7de7ab683009519926fbf954165f0
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2022-08-26 08:43:40 +0100

    Upgraded dependencies

--- app/uk/gov/hmrc/pdfgenerator/metrics/CountingMetrics.scala
+++ app/uk/gov/hmrc/pdfgenerator/metrics/CountingMetrics.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 HM Revenue & Customs
+ * Copyright 2022 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -117,7 +117,7 @@ class PdfGeneratorMetric @Inject()(val metrics: Metrics)
         logger.info(s"Getting free diskspace ${freeSpace}Mb")
         freeSpace
       } catch {
-        case e => {
+        case e: Throwable => {
           logger.error(s"DiskSpaceGuage: Bad Disk Space value ${e.getMessage}")
           -1
         }
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,31 +5,29 @@ object AppDependencies {
 
   import play.core.PlayVersion
 
+  val bootstrapVersion = "7.0.0"
+
   val compile: Seq[ModuleID] = Seq(
     ws,
     "org.reactivemongo"  %% "play2-reactivemongo"       % "1.0.7-play28",
-    "org.jsoup"           %  "jsoup"                    % "1.14.2",
-    "uk.gov.hmrc"        %% "bootstrap-backend-play-28" % "5.12.0",
-    "uk.gov.hmrc"        %% "domain"                    % "6.2.0-play-28",
+    "org.jsoup"           %  "jsoup"                    % "1.15.3",
+    "uk.gov.hmrc"        %% "bootstrap-backend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc"        %% "domain"                    % "8.1.0-play-28",
     "io.github.cloudify" %% "spdf"                      % "1.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"          % "3.0.8",
-    "com.vladsch.flexmark"    % "flexmark-all"       % "0.35.10",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0",
-    "org.pegdown"             % "pegdown"            % "1.6.0",
-    "com.typesafe.play"      %% "play-test"          % PlayVersion.current,
-    "org.mockito"             % "mockito-core"       % "3.6.0",
-    "org.apache.pdfbox"       % "pdfbox"             % "2.0.13"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion,
+    "org.pegdown"             % "pegdown"                % "1.6.0",
+    "com.typesafe.play"      %% "play-test"              % PlayVersion.current,
+    "org.mockito"             % "mockito-core"           % "4.7.0",
+    "org.apache.pdfbox"       % "pdfbox"                 % "2.0.26"
   ).map(_ % "test")
 
   val it: Seq[ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"          % "3.0.8",
-    "com.vladsch.flexmark"    % "flexmark-all"       % "0.35.10",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion,
     "org.pegdown"             % "pegdown"            % "1.6.0",
-    "com.typesafe.play"      %% "play-test"          % PlayVersion.current,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"
+    "com.typesafe.play"      %% "play-test"          % PlayVersion.current
   ).map(_ % "it")
 
   val all: Seq[ModuleID] = compile ++ test ++ it
